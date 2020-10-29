6d8d25000640e0f54268bfd8eea145f527414e57
vladislav-volodchenko <70842775+vladislav-volodchenko@users.noreply.github.com>
2020-10-29 16:54:52 +0300

    feat: complete task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+	let encrypt = '';
+	if (Array.isArray(arr)){
+		for (let i = 0; i < arr.length; i++) {
+			if (typeof(arr[i]) !== 'string') {
+				arr.splice(i, 1);
+				i--;
+			}
+		}
+		arr.flat().map(value => value.trim()).forEach(value => encrypt += value.slice(0,1).toUpperCase());
+	} else encrypt = false;
+	return encrypt ? encrypt.split('').sort().join('') : encrypt;
 };
